c1cfa3bc95763be616c006a47d522d0d6d02e9ef
ikuluev <ikuluev@MacBook-Pro-ikuluev.local>
2023-11-13 22:35:54 +0300

    fix +3333 days case

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ const { boolean } = require('yargs')
 
 const argv = yargs(hideBin(process.argv)).option(
     "year", {
-        alias: "y",
-        default: false
+        alias: "y"
     }
 ).option(
     "month", {
         alias: "m",
-        default: false
     }
 ).option(
     "date", {
         alias: "d",
-        default: false
     }
 ).argv
 
@@ -24,6 +21,7 @@ console.log(argv)
 
 const date = new Date()
 
+// получение текущего года, месяца, дня
 if (argv._.includes('current') && !argv.year && !argv.month && !argv.date) {
     console.log(date)
 } else if (argv._.includes('current') && argv.year && !argv.month && !argv.date) {
@@ -34,27 +32,34 @@ if (argv._.includes('current') && !argv.year && !argv.month && !argv.date) {
     console.log(date.getDate())
 }
 
+// получение года, месяца, дня в будущем
 // кейс когда задают + 1234 дня надо конвертировать в месяцы и годы
 if(argv._.includes('add') && argv.year && !argv.month && !argv.date) {
-    const newYear = date.getFullYear() + argv.year
-    date.setFullYear(newYear)
-    console.log(newYear)
+    const futureYearUnix = date.setFullYear(date.getFullYear() + argv.year)
+    const futureYearIso = new Date(futureYearUnix).toISOString()
+    console.log(futureYearIso)
 } else if (argv._.includes('add') && !argv.year && argv.month && !argv.date) {
-    const newMonth = date.getMonth() + 1 + argv.month
-    console.log(newMonth)
+    const futureMonthUnix = date.setMonth(date.getMonth() + argv.month)
+    const futureMonthIso = new Date(futureMonthUnix).toISOString()
+    console.log(futureMonthIso)
 } else if (argv._.includes('add') && !argv.year && !argv.month && argv.date) {
-    const newDate = date.getDate() + argv.date
-    console.log(newDate)
+    const futureDateUnix = date.setDate(date.getDate() + argv.date)
+    const futureDateIso = new Date(futureDateUnix)
+    console.log(futureDateIso)
 }
 
+// получение года, месяца, дня в прошлом
+// кейс когда задают -1234 дня надо конвертировать в месяцы и годы
 if(argv._.includes('sub') && argv.year && !argv.month && !argv.date) {
-    const newYear = date.getFullYear() - argv.year
-    date.setFullYear(newYear)
-    console.log(newYear)
+    const pastYearUnix = date.setFullYear(date.getFullYear() - argv.year)
+    const pastYearIso = new Date(pastYearUnix).toISOString()
+    console.log(pastYearIso)
 } else if (argv._.includes('sub') && !argv.year && argv.month && !argv.date) {
-    const newMonth = date.getMonth() + 1 - argv.month
-    console.log(newMonth)
+    const pastMonthUnix = date.setMonth(date.getMonth() - argv.month)
+    const pastMonthIso = new Date(pastMonthUnix).toISOString()
+    console.log(pastMonthIso)
 } else if (argv._.includes('sub') && !argv.year && !argv.month && argv.date) {
-    const newDate = date.getDate() - argv.date
-    console.log(newDate)
+    const pastDateUnix = date.setDate(date.getDate() - argv.date)
+    const pastDateIso = new Date(pastDateUnix)
+    console.log(pastDateIso)
 }
