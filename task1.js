167636259177472f3f058b41ac660f201390cac3
ikuluev <ikuluev@MacBook-Pro-ikuluev.local>
2023-11-17 13:23:00 +0300

    generation log dir and log file

--- task1.js
+++ task1.js
@@ -3,7 +3,10 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const readline = require("node:readline");
 const { stdin: input, stdout: output } = require("node:process");
+const fs = require("fs");
+const path = require("path");
 const Helper = require("./helper.js");
+const { error } = require("node:console");
 
 const helper = new Helper();
 
@@ -16,24 +19,38 @@ const argv = yargs(hideBin(process.argv)).option("logs", {
   demandOption: true,
 }).argv;
 
+console.log(argv);
+const dir = path.join(__dirname, "logs");
+const filePath = path.join(dir, argv.logs);
+
+// создать дирректорию logs, если ее нет
+if (!fs.existsSync(dir)) {
+  fs.mkdir(dir, (error) => {
+    if (error) throw Error(error);
+  });
+}
+
+const fileHeader = "GAME STARTED: " + new Date();
+fs.writeFile(filePath, fileHeader, (error) => {
+  if (error) throw Error(error);
+});
+
+// генерация рандомного числа
 const randomNumber = helper.getRandomInt(1, 3); // не включая 3
-console.log(randomNumber) // читы
+console.log(randomNumber); // читы
 
 console.log("ОРЕЛ И РЕШКА: отгадай случайное число (1 или 2) \n");
 
 function startGame() {
-  rl.question(
-    "Введите число: ",
-    (answer) => {
-      if (+answer !== randomNumber) {
-        console.log("Неверно");
-        startGame();
-      } else {
-        console.log("Ты выйграл!");
-        rl.close();
-      }
+  rl.question("Введите число: ", (answer) => {
+    if (+answer !== randomNumber) {
+      console.log("Неверно");
+      startGame();
+    } else {
+      console.log("Ты выйграл!");
+      rl.close();
     }
-  );
+  });
 }
 
 startGame();
