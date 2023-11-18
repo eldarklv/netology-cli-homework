a95ac64d408685875f5ace52a3e08e6181b9853a
Ildar Kuluev <ildar.l.kuluev63@gmail.com>
2023-11-19 01:13:46 +0300

    add async await

--- task1.js
+++ task1.js
@@ -6,7 +6,6 @@ const { stdin: input, stdout: output } = require("node:process");
 const fs = require("fs");
 const path = require("path");
 const Helper = require("./helper.js");
-const { error } = require("node:console");
 
 const helper = new Helper();
 
@@ -30,26 +29,62 @@ if (!fs.existsSync(dir)) {
   });
 }
 
-const fileHeader = "GAME STARTED: " + new Date();
-fs.writeFile(filePath, fileHeader, (error) => {
-  if (error) throw Error(error);
-});
+async function startGame() {
+  console.log("ОРЕЛ И РЕШКА: отгадай случайное число (1 или 2) \n");
 
-// генерация рандомного числа
-const randomNumber = helper.getRandomInt(1, 3); // не включая 3
-console.log(randomNumber); // читы
+  // создать дирректорию logs, если ее нет
+  if (!fs.existsSync(dir)) {
+    await fs.mkdir(dir, (error) => {
+      if (error) throw Error(error);
+    });
+  }
 
-console.log("ОРЕЛ И РЕШКА: отгадай случайное число (1 или 2) \n");
+  // записываем в логи, что игра началась
+  const logGameStarted = "GAME START " + new Date() + "\n";
+  await fs.appendFile(filePath, logGameStarted, (error) => {
+    if (error) throw Error(error);
+  });
 
-function startGame() {
-  rl.question("Введите число: ", (answer) => {
+  // генерация рандомного числа, запись в лог этого числа
+  const randomNumber = helper.getRandomInt(1, 3); // не включая 3
+  await fs.appendFile(
+    filePath,
+    `GENERATED randomNumber=${randomNumber} \n`,
+    (error) => {
+      if (error) throw Error(error);
+    }
+  );
+
+  console.log(randomNumber); // читы
+
+  // запрашиваем ответ от игрока, логгируем его действие 
+  let gameResult = "";
+  rl.question("Введите число: ", async (answer) => {
     if (+answer !== randomNumber) {
       console.log("Неверно");
-      startGame();
+      gameResult = "GAME LOSE \n";
+      await fs.appendFile(
+        filePath,
+        `USER ACTION wrong answer=${answer} \n`,
+        (error) => {
+          if (error) throw Error(error);
+        }
+      );
     } else {
       console.log("Ты выйграл!");
-      rl.close();
+      gameResult = "GAME WIN \n";
+      await fs.appendFile(
+        filePath,
+        `USER ACTION correct answer=${answer} \n`,
+        (error) => {
+          if (error) throw Error(error);
+        }
+      );
     }
+    await fs.appendFile(filePath, gameResult, (error) => {
+      if (error) throw Error(error);
+    });
+    rl.close();
   });
 }
 
